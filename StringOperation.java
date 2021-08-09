5fbf4d9d3a6eaac6f55b134b595d177cfc865289
bamsaengki <bamsaengki@gmail.com>
2021-08-09 19:59:26 +0900

    문자열

---
+++ StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow World".length());
+		System.out.println("Hello,[[[name]]] ... bye. " .replace("[[[name]]]", "bam"));
+
+	}
+
+}
